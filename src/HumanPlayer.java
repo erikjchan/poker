3618d86b5f64c9e947884dcb14295f177d462fde
ejc233 <ejc233@cornell.edu>
2017-04-03 05:13:46 -0400

    renamed User to HumanPlayer for consistency

---
+++ src/HumanPlayer.java
@@ -0,0 +1,17 @@
+/**
+ * Class that represents a human player of the game.
+ * 
+ * @author echan
+ */
+public class HumanPlayer extends Player {
+
+	/**
+	 * Constructor class.
+	 * 
+	 * @param name
+	 *            the name of the human player
+	 */
+	public HumanPlayer(String name) {
+		super(name);
+	}
+}
